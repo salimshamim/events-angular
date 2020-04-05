8759f8bb50bd8662981ef5762f2fa755c399e469
unknown <salim.shamim.94@gmail.com>
2020-04-05 19:49:13 +0530

    Linking to routes (navigate by adding a link in html [routerLink])

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { EventsAppComponent } from './events-app.component';
 import {EventsListComponent} from './events/events-list.component';
+import {EventDetailsComponent} from './events/event-details.component';
 import {EventThumbNailComponent} from './events/event-thumbnail.component';
 import {NavBarComponent} from './nav/navbar.component';
 import {EventsService} from './events/shared/events.service';
+import {RouterModule} from '@angular/router';
+import {routes} from './appRoutes';
 
 @NgModule({
   declarations: [
     EventsAppComponent,
     EventsListComponent,
     EventThumbNailComponent,
+    EventDetailsComponent,
     NavBarComponent
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [EventsService],
   bootstrap: [EventsAppComponent]
---
+++ src/app/events/event-details.component.ts
@@ -0,0 +1,27 @@
+import {Component, OnInit} from '@angular/core';
+import {EventsService} from './shared/events.service';
+import {ActivatedRoute} from '@angular/router'
+
+@Component({
+    templateUrl:'/event-details.html',
+    styles:[`
+        .container {padding-left:20px; padding-right: 20px;}
+        .event-image {height:100px}    
+    `]
+})
+
+export class EventDetailsComponent implements OnInit{
+
+    event: any;
+
+    ngOnInit(){
+        let eventId = this.activatedRoute.snapshot.params['id'];
+        console.log(eventId)
+        this.event = this.eventService.getEvent(+eventId);
+    }
+
+    constructor(private eventService:EventsService,private activatedRoute:ActivatedRoute){
+
+    }
+    
+}
